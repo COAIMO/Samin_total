5e486ecac1c43e131582a2033996d87691852f7b
ckdo8008 <68054358+ckdo8008@users.noreply.github.com>
2023-12-05 19:32:59 +0900

    메모리 누수 및 스레드 안전성 향상 중

--- app/src/main/java/com/coai/samin_total/Oxygen/OxygenMainFragment.kt
+++ app/src/main/java/com/coai/samin_total/Oxygen/OxygenMainFragment.kt
@@ -25,7 +25,6 @@ import com.coai.samin_total.databinding.FragmentOxygenMainBinding
 import java.util.*
 import java.util.concurrent.atomic.AtomicBoolean
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
